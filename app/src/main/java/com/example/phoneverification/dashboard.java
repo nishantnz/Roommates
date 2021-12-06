931b689e3e2cb0b98bb3100bf562a6d43f9353ef
nishantnz <nishant.z@somaiya.edu>
2021-12-06 21:54:16 +0530

    Replace setOnNavigationItemSelectedListener's anonymous class with lambda

--- app/src/main/java/com/example/phoneverification/dashboard.java
+++ app/src/main/java/com/example/phoneverification/dashboard.java
@@ -2,9 +2,7 @@ package com.example.phoneverification;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -20,23 +18,20 @@ public class dashboard extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.home);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.home:
-                        return true;
-                    case R.id.profile:
-                        startActivity(new Intent(getApplicationContext(), profile.class));
-                        overridePendingTransition(22, 1);
-                        return true;
-                    case R.id.chat:
-                        //startActivity(new Intent(getApplicationContext(), logIn.class));
-                        overridePendingTransition(22, 0);
-                        return true;
-                }
-                return false;
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    return true;
+                case R.id.profile:
+                    startActivity(new Intent(getApplicationContext(), profile.class));
+                    overridePendingTransition(22, 1);
+                    return true;
+                case R.id.chat:
+                    //startActivity(new Intent(getApplicationContext(), logIn.class));
+                    overridePendingTransition(22, 0);
+                    return true;
             }
+            return false;
         });
     }
 }
