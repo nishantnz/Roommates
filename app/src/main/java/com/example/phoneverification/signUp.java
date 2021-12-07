ae3fb855140b5d478fe7cf465a41b298270b87f8
nishantnz <nishant.z@somaiya.edu>
2021-12-08 02:35:32 +0530

    added intent.putextra in signup page for profile

--- app/src/main/java/com/example/phoneverification/signUp.java
+++ app/src/main/java/com/example/phoneverification/signUp.java
@@ -1,8 +1,5 @@
 package com.example.phoneverification;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,6 +8,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -43,7 +43,7 @@ public class signUp extends AppCompatActivity {
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),logIn.class));
+                startActivity(new Intent(getApplicationContext(), logIn.class));
             }
         });
 
@@ -51,49 +51,46 @@ public class signUp extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-               //register user using firebase
-                String emailo =  emailOne.getText().toString().trim();
-                String passwordo =  passwordOne.getText().toString().trim();
-                String confirmpasswordo =  confirmPassword.getText().toString().trim();
+                //register user using firebase
+                String emailo = emailOne.getText().toString().trim();
+                String passwordo = passwordOne.getText().toString().trim();
+                String confirmpasswordo = confirmPassword.getText().toString().trim();
 
-                    if(TextUtils.isEmpty(emailo)) {
-                        emailOne.setError("Please Enter Email");
-                        return;
-                    }
-                    else if(TextUtils.isEmpty(passwordo)) {
-                        passwordOne.setError("Please Enter Password");
-                        return;
-                    }
-                    else if(TextUtils.isEmpty(confirmpasswordo)) {
-                        confirmPassword.setError("Please Confirm your password");
-                        return;
-                    }
-                    else if(passwordo.trim().length() < 6) {
-                        passwordOne.setError("Password should be atleast 6 characters");
-                        return;
-                    }
-                    else if(!passwordo.equals(confirmpasswordo)) {
-                        confirmPassword.setError("Password should match,Try again");
-                        return;
-                    }
+                if (TextUtils.isEmpty(emailo)) {
+                    emailOne.setError("Please Enter Email");
+                    return;
+                } else if (TextUtils.isEmpty(passwordo)) {
+                    passwordOne.setError("Please Enter Password");
+                    return;
+                } else if (TextUtils.isEmpty(confirmpasswordo)) {
+                    confirmPassword.setError("Please Confirm your password");
+                    return;
+                } else if (passwordo.trim().length() < 6) {
+                    passwordOne.setError("Password should be atleast 6 characters");
+                    return;
+                } else if (!passwordo.equals(confirmpasswordo)) {
+                    confirmPassword.setError("Password should match,Try again");
+                    return;
+                }
 
-                   sAuth.createUserWithEmailAndPassword(emailo,confirmpasswordo).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                       @Override
-                       public void onComplete(@NonNull Task<AuthResult> task) {
-                     if(task.isSuccessful())
-                         {
+                sAuth.createUserWithEmailAndPassword(emailo, confirmpasswordo).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
                             Toast.makeText(getApplicationContext(), "Thanks for Signing Up", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(signUp.this, profile.class));
+                            Intent intent = new Intent(signUp.this, profile.class);
+                            intent.putExtra("source", "signup");
+                            startActivity(intent);
                             finishAffinity();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Error! " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
                         }
-                    else{
-                        Toast.makeText(getApplicationContext(), "Error! "+ Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
-                    }
                     }
 
-            });
-        }
-    });
+                });
+            }
+        });
 
 
-}}
+    }
+}
